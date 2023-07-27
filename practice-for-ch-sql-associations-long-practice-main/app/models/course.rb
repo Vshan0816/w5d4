ace9db2b244d49a14102dbc4a4d09cd2b1cc5f90
Naim Miah <mmiah@fordham.edu>
2023-07-27 15:36:44 -0400

    almost completed rails association classwork

--- practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
@@ -1,2 +1,32 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string           not null
+#  prereq_id     :bigint
+#  instructor_id :bigint           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Course < ApplicationRecord
+  has_many :enrollments,
+    class_name: 'Enrollment',
+    foreign_key: :course_id,
+    primary_key: :id
+
+    belongs_to :prerequisite,
+    class_name: 'Course',
+    foreign_key: :prereq_id,
+    primary_key: :id,
+    optional: true
+
+    belongs_to :instructor,
+    class_name: 'User',
+    foreign_key: :instructor_id,
+    primary_key: :id
+
+
+
+    has_many :students, through: :enrollments, source: :user
 end
--- practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
@@ -1,2 +1,23 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+  has_many :enrollments,
+    class_name: :Enrollment,
+    foreign_key: :student_id,
+    primary_key: :id
+
+    has_many :taught_courses,
+    class_name: :Course,
+    foreign_key: :instructor_id,
+    primary_key: :id
+
+
+    has_many :courses, through: :enrollments, source: :course
 end
